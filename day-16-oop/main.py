35a6ab8ea5d064e62c2efdaaa6bf7c065b7a76cb
ffyilmazz <ffurkanyilmazz@gmail.com>
2023-01-31 17:39:15 +0300

    Section 16: OOP - Lecture 147. Constructing Objects and Accessing their Attributes and Methods

--- day-16-oop/main.py
+++ day-16-oop/main.py
@@ -1,5 +1,22 @@
-print("Hello!")
-print("This is new change")
-print("This 3rd change!")
-print("This 4th change!")
+# import modules
+import turtle
+# resources:
+# 1- https://docs.python.org/3/library/turtle.html
+# 2- https://cs111.wellesley.edu/reference/colors
 
+# creating an object from class
+timmy = turtle.Turtle()
+print(timmy)
+
+# methods belong to created object
+timmy.color("coral")
+timmy.shape("turtle")
+timmy.forward(100)
+
+my_screen = turtle.Screen()
+
+# accessing attributes
+print(my_screen.canvheight)
+
+# methods belong to created object
+my_screen.exitonclick()
